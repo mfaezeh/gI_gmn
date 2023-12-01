6eadd48f764c22191ceee6137e961c21782a15e4
Faezeh Moradi <fmoradi@waverley.CS.Dal.Ca>
2023-12-01 16:03:48 -0400

    First commit

---
+++ src/gi_gmn/models.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class GroupSubjectGMN():
+    def __init__(self):
+        print("Model: Group-Subject GMN")
+        return
+
+    def group_level(self):
+        return
+
+    def individual_level(self):
+        return
+
+    def prune_connections(self):
+        return
+
+    def fit(self):
+        self.group_level()
+        self.prune_connections()
+        self.individual_level()
+        print("Model fitted successfully!")
+
---
+++ src/gi_gmn/utils.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pandas as pd
+import nibabel as nib
+
+def get_subject(path1 ,path2, masker, TR, NRecord):  # two record version
+    file_GM = nib.load(path1)             
+    file_data1 = nib.load(path1).get_fdata()[:, :, :, :TR]
+    file_data2 = nib.load(path2).get_fdata()[:, :, :, :TR]
+    
+    ins1, ins2, ins3 = file_data1.shape[0], file_data1.shape[1], file_data1.shape[2]
+    # chekc larg time points
+    # if file_data1.shape[3] >170 or file_data2.shape[3] > 170:
+    #     print('one')
+        
+    ins = ins1 * ins2 * ins3
+    print(file_data1.shape)
+
+    file_data1 = file_data1.reshape(ins, TR)
+    file_data2 = file_data2.reshape(ins, TR)
+    all_ = np.concatenate((file_data1, file_data2), axis = 1)
+
+    file_data = all_.reshape((ins1, ins2, ins3, TR * NRecord))
+    print(file_data.shape, all_.shape)
+
+    fileo = nib.Nifti1Image(file_data, file_GM.affine)  
+    DMN_signals = masker.fit_transform(fileo)
+    # print(DMN_signals.shape,NR)
+    
+    #Normalize time series of each ROI independetly with mean of one and std of zero
+    for i in range(ROI):
+        mu = np.mean(DMN_signals[:, i], axis = 0)
+        std = np.std(DMN_signals[:, i], axis = 0)
+        DMN_signals[:, i] = (DMN_signals[:, i] - mu) / std
+    
+    return DMN_signals
+
+
+def flatten_matrix(d_):
+     d = pd.DataFrame(d_)
+     d_masked = d.mask(np.tril(np.ones(d.shape)).astype(np.bool))
+     #                 print(d_masked)
+     d_masked = d_masked[abs(d_masked) >= 0].stack().reset_index()
+     #                 print(d_masked.shape)
+     #                 print(type(d_masked))
+
+     # to only consider the last column for the feature set * feature meaning here *
+     #                 print(d_masked.columns)
+     d_masked = d_masked [[0]]
+     #             print(d_masked[0].values.tolist())
+     corr_list = d_masked[0].values.tolist()
+     corr_np = np.array(corr_list)
+     # print(corr_np.shape)
+     return corr_np
+
+def get_seed_id(str_, all):
+    all = np.array(all)
+#     print(all)
+    id_str = np.where(all == str_)[0][0]
+#     print(id_str)
+    d_ = np.ones((all.shape[0], all.shape[0]))
+    d = pd.DataFrame(d_)
+    d_masked = d.mask(np.tril(np.ones(d.shape)).astype(np.bool))
+    # print(d_masked)
+    d_masked = d_masked[abs(d_masked) >= 0].stack().reset_index()
+    level0 = d_masked['level_0'].values
+    level1 = d_masked['level_1'].values
+
+    d_masked = d_masked [[0]]
+
+    corr_list = d_masked[0].values.tolist()
+
+    list_str0 = np.where( level0 == id_str)[0]
+    list_str1 = np.where(level1 == id_str)[0]
+
+    return np.concatenate((list_str0, list_str1))
+
+def get_labels(i, arr):
+    all_data = np.array(arr)
+    d_ = np.ones((len(arr), len(arr)))
+    d = pd.DataFrame(d_)
+    d_masked = d.mask(np.tril(np.ones(d.shape)).astype(np.bool))
+    # print(d_masked)
+    d_masked = d_masked[abs(d_masked) >= 0].stack().reset_index()
+    level0 = d_masked['level_0'].values
+    level1 = d_masked['level_1'].values
+
+    x = level0[i]
+    y = level1[i]
+    # print(x, type(all[x] ))
+    corr_pair = all_data[x] + " " + all_data[y]
+
+    d_masked = d_masked [[0]]
+    #             print(d_masked[0].values.tolist())
+    corr_list = d_masked[0].values.tolist()
+
+    return corr_pair
+
+# TODO: The return statement only activates after the if, is that correct?
+
+def get_correlation_indexes(str1, str2, all_data):
+    fg = get_seed_id(str1, all)#,get_seed_id('Precuneus_R')
+    for i in range(len(fg)):
+        # print(get_lables(fg[i]))
+        if get_lables(fg[i], all_data) == str1 + " " + str2 or get_labels(fg[i], all_data) == str2 + " " + str1:
+            # print(fg[i])
+            return fg[i]
+
+def split_into_chunks(arr, chunk_size):
+    print("DEBUG")
+    return [arr[i:i + chunk_size] for i in range(0, len(arr), chunk_size)]
+
+def degree_sparsity(arr):
+    size = (arr.shape[0] - 1) * (arr.shape[0] / 2)
+    return np.count_nonzero(arr) / (2 * size)
